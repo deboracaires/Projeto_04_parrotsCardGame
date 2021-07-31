a7cc8d30caadd70c11fe1210cd3a7899fca1c4f3
deboracaires <debora.caires.mor@gmail.com>
2021-07-31 13:19:26 -0300

    layout arrumado, carta com parte frente e verso sem embaralhamento

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,6 @@
 let verificaNumeroCartas = 0;
 let numeroCartas = 0;
+const cartas = document.querySelectorAll(".carta");
 
 while(verificaNumeroCartas === 0){
 numeroCartas = prompt("Deseja jogar com quantas cartas?(digite apenas o número)\n- 4 cartas\n- 6 cartas\n- 8 cartas\n- 10 cartas\n- 12 cartas\n- 14 cartas");
@@ -15,10 +16,25 @@ if( numeroCartas%2 === 0 && numeroCartas >= 4 && numeroCartas <= 14){
 function inserirCartas(){
     const ul = document.querySelector("ul");
     for(let i = 0; i < numeroCartas; i++){
-        ul.innerHTML += `<button class="carta" onclick="virarCarta(this)"><li>
-        <img src="/images/carrot.png">
-        </li></button>`;
+        ul.innerHTML += `
+        <div class="vira-carta">
+            <div class="carta">
+                <div class="carta-frente">
+                    <img src="/images/front.png">
+                </div>
+                <div class="carta-verso">
+                    <img src="/images/bobrossparrot.gif">
+                </div>
+            </div>
+        </div>`;
     }
 }
+// function virarCarta(){
+//     this.classList.toggle("virar-carta");
+// }
+// cartas.forEach(carta => carta.addEventListener('click', virarCarta))
+
+
+
 
 
